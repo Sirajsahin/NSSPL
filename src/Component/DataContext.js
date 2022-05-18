123e09466811a2033b2f965bc4993f8fd343fe71
Sirajsahin <96188626+Sirajsahin@users.noreply.github.com>
2022-05-18 17:04:03 +0530

    Add files via upload

--- src/Component/DataContext.js
+++ src/Component/DataContext.js
@@ -5,16 +5,17 @@ const password = "pass";
 const DataContext = createContext({});
 export const DataProvider =({children}) =>{
     const [data, setData] = useState([]);
+    console.log(data)
     const [input, setInput] = useState("");
     const [edit, setEdit] = useState(0);
-    const [flage, setFlage] = useState(true);
+    const [flage, setFlage] = useState(false);
     const [inde, setInde] = useState([]);
     const [name, setName] = useState("");
     const [pass, setPass] = useState("");
     const [show,setShow]=useState(false)
     const [onload,setOnload]=useState(true)
-  
-
+    
+      
 
     const addDatatoLocalStorage = async () => {
         try {
@@ -48,10 +49,53 @@ export const DataProvider =({children}) =>{
         getDataFromStorage();
       }, []);
 
+      const handelSubmit = (e) => {
+        e.preventDefault();
+       
+        if (flage) {
+          let items = [...data];
+          let item = items[edit];
+          item.login = input;
+          items[edit] = item;
+          setData(items);
+          setEdit(0);
+          setInput("");
+          setFlage(false);
+        }
+    
+        if (!flage) {
+          setData((val) => [...val, { ["login"]: input }]);
+          setInput("");
+         }
+      };
+     
+      const deleteElm = (id) => {
+        const update = data.filter((val, ind) => {
+          return ind !== id;
+        });
+        setData(update);
+      };
+      const editElm = (val, id) => {
+        setInput(val.login);
+        setEdit(id);
+        setFlage(true);
+      };
+    const logoutUser=()=>{
+        setShow(false)
+    }
+    const setEmpolyee=(value)=>{
+        setInde(value)
+        setOnload(false)
+    }
+    useEffect(()=>{
+      localStorage.setItem('data',JSON.stringify(data));
+   },[data])
 
     const handelSubmitLoin = (e) => {
       e.preventDefault();
-      if (name === userName && pass === password) {
+      const names=name.toLowerCase()
+      const passs=pass.toLowerCase()
+      if (names === userName && passs === password) {
         console.log("match");
         setName("");
         setPass("");
@@ -64,45 +108,7 @@ export const DataProvider =({children}) =>{
       }
     };
 
- 
    
-    const deleteElm = (id) => {
-      const update = data.filter((val, ind) => {
-        return ind !== id;
-      });
-      setData(update);
-    };
-    const editElm = (val, id) => {
-      setInput(val.login);
-      setEdit(id);
-      setFlage(true);
-    };
-  const logoutUser=()=>{
-      setShow(false)
-  }
-  const setEmpolyee=(value)=>{
-      setInde(value)
-      setOnload(false)
-  }
-  const handelSubmit = (e) => {
-    e.preventDefault();
-   
-    if (flage) {
-      let items = [...data];
-      let item = items[edit];
-      item.login = input;
-      items[edit] = item;
-      setData(items);
-      setEdit(0);
-      setInput("");
-      setFlage(false);
-    }
-
-    if (!flage) {
-      setData((val) => [...val, { ["login"]: input }]);
-      setInput("");
-    }
-  };
   return (
     <div>
       <DataContext.Provider
